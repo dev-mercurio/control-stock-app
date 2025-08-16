da16c40750079b3ab836763a01df5499c5750cc5
maxi <usuario@MacBook-Pro-de-usuario.local>
2025-08-16 01:53:38 -0300

    linter

--- App.js
+++ App.js
@@ -1,19 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image, Button, TouchableHighlight } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  Button,
+  TouchableHighlight,
+} from "react-native";
 
-import icon from './assets/favicon.png';
+import icon from "./assets/favicon.png";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Image source={icon} style={{width: 100, height: 100}} />
+      <Image source={icon} style={{ width: 100, height: 100 }} />
       <StatusBar style="auto" />
       <Text>Hello world!</Text>
-      <Button title="Pulsa aquí" onPress={() => alert('Hola!')}/>
+      <Button title="Pulsa aquí" onPress={() => alert("Hola!")} />
       <TouchableHighlight
         underlayColor={"#09f"}
-        onPress={() => alert('Chau!')}
-        style={{ width: 200, height: 200}}
+        onPress={() => alert("Chau!")}
+        style={{ width: 200, height: 200 }}
       >
         <Text>También puedes pulsar aquí</Text>
       </TouchableHighlight>
@@ -24,8 +31,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#09f',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#09f",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
